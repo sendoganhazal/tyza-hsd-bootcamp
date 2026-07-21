99eae48c1f7304dea35d789ee550d81822962b79
Hazal Sendogan <sendoganhazal@gmail.com>
2026-07-21 13:00:37 +0300

    1. Pandas Kütüphanesine Giriş

---
+++ pyhton/09_pandas/01_intro.py
@@ -0,0 +1,29 @@
+"""
+Pandas: veri bilimi kütüphanesi
+    - tablo şeklinde veri oluşturmak
+    - verileri düzenlemek, filtrelemek
+    - sütun ve satır işlemleri yapmak 
+    - dosyalardan veri okumak
+
+Pandas Numpy ilişkisi:
+    - numpy: sayısal dizi sağlar
+    - pandas: tablo veri yapıları
+
+Pandas Nerelerde Kullanılır?
+    - veri analizi
+    - veri düzenleme
+    - veri temizleme
+    - veri işleme
+    - veri dosyalarını okuma
+
+Pandas ile ilgili neler öğreneceğiz?
+    - Series
+    - dataframe
+    - veri okuma ve yazma
+    - veri seçme ve filtreleme
+    - sütun ve satır işlemleri
+    - veri sıralama ve gruplama
+    - temel pandas fonksiyonları
+"""
+
+# import pandas as pd
